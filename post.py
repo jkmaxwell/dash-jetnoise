7b9661451ee4e731f46d276e7e4f201818fc417f
Justin Maxwell <soc@code404.com>
2016-07-17 19:17:43 -0700

    fixing dumb errors

--- post.py
+++ post.py
@@ -1,6 +1,6 @@
 import requests
 url = "https://stop.jetnoise.net/add-complaint"
-payload = {'browser_uuid': 'GET_UUID_FROM_BROWSER',
+payload = {'browser_uuid': 'ENTER_UUID',
            'browser_name': 'Chrome',
            'browser_version': '51',
            'browser_vendor': 'Google Inc.',
@@ -8,18 +8,18 @@ payload = {'browser_uuid': 'GET_UUID_FROM_BROWSER',
            'content': '',
            'activity': 'Quality+of+Life',
            'loudness': '2'}
-headers = {'accept': 'text/html,application/xhtml+xml,\
-                      application/xml;q=0.9,image/webp,*/*;q='0.8,
+headers = {'accept': 'text/html,application/xhtml+xml,application/xml;\
+            q=0.9,image/webp,*/*;q=0.8',
            'accept-encoding': 'gzip, deflate, br',
            'accept-language': 'en-US,en;q=0.8',
            'cache-control': 'no-cache',
            'content-length': '171',
            'content-type': 'application/x-www-form-urlencoded',
-           'cookie': 'GET_COOKIE_FROM_BROWSER',
+           'cookie': 'ENTER_COOKIE',
            'dnt': '1',
-           'origin': 'https': '//stop.jetnoise.net',
+           'origin': 'https://stop.jetnoise.net',
            'pragma': 'no-cache',
-           'referer': 'https': '//stop.jetnoise.net/',
+           'referer': 'https://stop.jetnoise.net/',
            'upgrade-insecure-requests': '1',
            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 \
